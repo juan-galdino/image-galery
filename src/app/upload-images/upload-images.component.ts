7bc29043f082a5596cbb863d1c88714e403fa93c
Juan Galdino <jgaldino.cs@gmail.com>
2023-07-07 15:36:02 -0300

    Added upload progress bar

--- src/app/upload-images/upload-images.component.ts
+++ src/app/upload-images/upload-images.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FirebaseStorageService } from '../firebase-storage.service';
+import { Observable, Subscription } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-upload-images',
   templateUrl: './upload-images.component.html',
   styleUrls: ['./upload-images.component.css']
 })
-export class UploadImagesComponent {
+export class UploadImagesComponent implements OnInit, OnDestroy {
   selectedFile: File | null = null
+  $uploadProgress!: Observable<number | undefined>
+  isUploading = false
+  message!: string
+  messageSubscription!: Subscription
 
-  constructor(private firebaseStorageService: FirebaseStorageService) {}
+  constructor(private storage: AngularFireStorage, private firebaseStorageService: FirebaseStorageService) {}
+
+  ngOnInit(): void {
+    this.messageSubscription = this.firebaseStorageService.messageSubject.subscribe(value => {
+      this.message = value
+    })  
+  }
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0]
@@ -17,12 +29,13 @@ export class UploadImagesComponent {
 
   onUploadFile() {
     if(this.selectedFile) {
+      this.isUploading = true
       const filePath = 'uploads/' + crypto.randomUUID() + this.selectedFile.name
-      this.firebaseStorageService.uploadFile(this.selectedFile, filePath).then(
-        url => console.log('Arquivo enviado com sucesso!', url )
-      ).catch(
-        error => console.error('Erro ao enviar arquivo:', error )
-      )
+      this.$uploadProgress = this.firebaseStorageService.uploadFile(this.selectedFile, filePath)
     }
+  }
+
+  ngOnDestroy(): void {
+    this.messageSubscription.unsubscribe()
   }
 }
