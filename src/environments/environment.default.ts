259f24aefea7190214535fe95d61c055a0a6fca8
Juan Galdino <jgaldino.cs@gmail.com>
2023-07-28 18:39:52 -0300

    Refactored environment files

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,13 @@
+export const defaultEnvironment = {
+  firebase: {
+    projectId: 'image-galery-ng',
+    appId: '1:827429489463:web:5ff8efa187abd4828568b2',
+    storageBucket: 'image-galery-ng.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD8JxnKwjmXgX4Ma_a6hoLT0X6v8Uf1ZbE',
+    authDomain: 'image-galery-ng.firebaseapp.com',
+    messagingSenderId: '827429489463',
+    measurementId: 'G-688S02489L',
+  },
+  production: false
+}
