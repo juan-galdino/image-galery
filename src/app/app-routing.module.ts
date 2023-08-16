fed8ecf27226e4f010c7849ba1a47f93a4c0e7c5
Juan Galdino <jgaldino.cs@gmail.com>
2023-08-16 18:04:49 -0300

    Landing page flow: Hero section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "home/galeria" },
+  { path: "", pathMatch: "full", loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
   { path: "login", loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: "cadastro", loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule) },
   { path: "home", loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
