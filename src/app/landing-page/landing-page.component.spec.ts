fa0b7162a01adf300342ddc820f1b3d1a1782c42
Juan Galdino <jgaldino.cs@gmail.com>
2023-08-18 12:49:06 -0300

    LP: App screenshot and Features section structure

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +9,8 @@ describe('LandingPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LandingPageComponent]
+      declarations: [LandingPageComponent],
+      imports: [MatIconModule]
     });
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
